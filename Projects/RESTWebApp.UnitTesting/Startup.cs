f6ed076cdb7e547d4e9a3873b08288f0791c77e4
Bayu Budianto <Bayu.Budianto@bni.co.id>
2023-12-01 15:43:36 +0700

    menambahkan unit testing (XUnit, Moq dan InMemoryDatabase EFCore)

---
+++ Projects/RESTWebApp.UnitTesting/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RESTWebApp.Application.ConfigProfile;
+using RESTWebApp.Application.Services.EmployeeService;
+using RESTWebApp.Data.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTWebApp.UnitTesting
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDbContext<HumanResourcesContext>(option =>
+                    option.UseInMemoryDatabase("Server=FAIRUZ-PC\\SQLEXPRESS;Database=SalesDB;Trusted_Connection=True;TrustServerCertificate=True;"));
+
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ConfigurationProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            // Add services to the container.
+            serviceCollection.AddSingleton(mapper);
+            serviceCollection.AddTransient<IEmployeeAppService, EmployeeAppService>();
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public ServiceProvider ServiceProvider { get; private set; }
+    }
+}
---
+++ Projects/RESTWebApp.UnitTesting/UnitServices/EmployeeServiceUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Moq;
+using RESTWebApp.Application.Helpers;
+using RESTWebApp.Application.Services.EmployeeService;
+using RESTWebApp.Application.Services.EmployeeService.Dto;
+using RESTWebApp.Data.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTWebApp.UnitTesting.UnitServices
+{
+    public class EmployeeServiceUnitTest : Mock<IEmployeeAppService>
+    {
+        [Fact]
+        public EmployeeServiceUnitTest GetAllProduct()
+        {
+            PageInfo pageInfo = new PageInfo(1, 5);
+            pageInfo.Page = 1;
+            pageInfo.PageSize = 5;
+
+            PagedResult<EmployeeListDto> pagedResult = new PagedResult<EmployeeListDto>();
+
+            Setup(x => x.GetAllEmployees(pageInfo))
+                 .ReturnsAsync(pagedResult);
+
+            return this;
+        }
+    }
+}
