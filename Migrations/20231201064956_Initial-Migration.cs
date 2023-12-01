2ffad581f638082cc5b0abeddd4aca8a9b2fda76
Bayu Budianto <Bayu.Budianto@bni.co.id>
2023-12-01 13:54:22 +0700

    perbaikan globalization dan migrations

---
+++ Migrations/20231201064956_Initial-Migration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RESTWebApp.Example.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "MstEmployee",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "Nvarchar(10)", nullable: false),
+                    Name = table.Column<string>(type: "Nvarchar(100)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MstEmployee", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MstEmployee",
+                schema: "dbo");
+        }
+    }
+}
