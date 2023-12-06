5c81c32cb8863d23e93f1bb18dc102e86540301c
Bayu Budianto <Bayu.Budianto@bni.co.id>
2023-12-06 15:57:45 +0700

    penambahan exception

---
+++ Projects/RESTWebApp.Application/Exceptions/ErrorException.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTWebApp.Application.Exceptions
+{
+    public class ErrorException: Exception
+    {
+        public ErrorException()
+        {
+
+        }
+
+        public ErrorException(string message)
+            : base(message)
+        {
+
+        }
+        public ErrorException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+
+        public override string HelpLink
+        {
+            get
+            {
+                return "Error occured! Call your Administrator for further information.";
+            }
+        }
+    }
+}
